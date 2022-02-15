b1b28d677fba8a22a4ba2a8e8b60652a51ec0406
Shubham-k <shubham1126.cse19@chitkara.edu.in>
2022-02-16 01:38:48 +0530

    fixed server bug 2

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
 
-const PATH = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 //setup handlebars engine and view location
 app.set("view engine", "hbs");
