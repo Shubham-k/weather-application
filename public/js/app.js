151904c5695b2414c9e3da1262b3c81fbf113e52
Shubham-k <shubham1126.cse19@chitkara.edu.in>
2022-02-16 01:42:56 +0530

    server bug 3

--- public/js/app.js
+++ public/js/app.js
@@ -10,20 +10,18 @@ const message6 = document.querySelector("#message-6");
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const location = input.value;
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          message1.textContent = data.error;
-        } else {
-          message1.textContent = data.address;
-          message2.textContent = `Temperature :- ${data.temperatureToday}`;
-          message3.textContent = `Minimum Temperature :- ${data.minimumTempToday}`;
-          message4.textContent = `Maximum Temperature :- ${data.maximumTempToday}`;
-          message5.textContent = `Humidity Level :- ${data.humidityLevel}`;
-          message6.textContent = `Overhaul :- ${data.main}`;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        message1.textContent = data.error;
+      } else {
+        message1.textContent = data.address;
+        message2.textContent = `Temperature :- ${data.temperatureToday}`;
+        message3.textContent = `Minimum Temperature :- ${data.minimumTempToday}`;
+        message4.textContent = `Maximum Temperature :- ${data.maximumTempToday}`;
+        message5.textContent = `Humidity Level :- ${data.humidityLevel}`;
+        message6.textContent = `Overhaul :- ${data.main}`;
+      }
+    });
+  });
 });
